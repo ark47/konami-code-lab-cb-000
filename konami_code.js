f084fe549d5791e48a102199c3610499749750d4
Chris Nunez <cenunez47@gmail.com>
2017-11-07 21:51:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,16 +6,16 @@ function init() {
   document.body.addEventListener('keydown', function(event) {
     var stroke = parseInt(event.detail); //stroke is defined as an integer of the keydown event
 
-    if (code[breaker] === stroke) { //if the key pressed is equal to the array index increment i by 1
+    if (code[breaker] === stroke) { //if the key pressed is equal to the array index increment breaker by 1
       breaker++;
 
-      if (breaker === code.length) { //if i integer is equal the length of the code array alert the user they completed the code and reset i to 0
+      if (breaker === code.length) { //if breaker integer is equal the length of the code array, alert the user they completed the code and reset i to 0
         alert('Hurray!');
         breaker = 0;
       }
     }
 
-    else { //if key pressed is not equal the code array index, reset i to 0
+    else { //if key pressed is not equal the code array index, reset breaker to 0
       breaker = 0;
     }
 
